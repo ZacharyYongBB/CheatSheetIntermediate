4865f9233ebd3de74f1ac7dee55901fc1fc8b519
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-10-16 17:08:14 +0800

    CoreData(MVVM)

--- CheatSheetIntermediate/CheatSheetIntermediateApp.swift
+++ CheatSheetIntermediate/CheatSheetIntermediateApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CheatSheetIntermediateApp: App {
     var body: some Scene {
         WindowGroup {
-            LocalNotificationExample()
+            CoreDataExample()
         }
     }
 }
